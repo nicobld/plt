b98d152ae2a833b5db1dc0e3f1b1ffe3ddda4cb8
JonahPichard <jnhp29@gmail.com>
2022-10-18 16:48:48 +0200

    changement main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -13,32 +13,33 @@ void testSFML() {
 
 using namespace std;
 using namespace state;
+using namespace sf;
 
 int main(int argc,char* argv[])
 {
-    //bonjour, c'est un test de branche
     Exemple exemple;
     exemple.setX(53);
     
     cout << "It works !" << endl;
 
         // création de la fenêtre
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
+    RenderWindow window(VideoMode(800, 600), "Catan");
+    window.setVerticalSyncEnabled(true);
+    
     // on fait tourner le programme tant que la fenêtre n'a pas été fermée
     while (window.isOpen())
     {
         // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
             // fermeture de la fenêtre lorsque l'utilisateur le souhaite
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         // effacement de la fenêtre en noir
-        window.clear(sf::Color::Blue);
+        window.clear(Color::Black);
 
         // c'est ici qu'on dessine tout
         // window.draw(...);
