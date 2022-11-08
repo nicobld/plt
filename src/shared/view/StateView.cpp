62435e0db0e991b4f60b656cc827ed43a6390da9
nicobld <belland.nicolas77@gmail.com>
2022-11-08 23:50:32 +0100

    added working tokens

--- src/shared/view/StateView.cpp
+++ src/shared/view/StateView.cpp
@@ -8,10 +8,9 @@ StateView::StateView(state::State& state) : state(state){
     int height = 720;
     displayHUD = new DisplayHUD(width, height, state.players[0], state.players[1], state.players[2], state.players[3]);
     tileMap = new TileMap();
-    renderPieces = new RenderPieces(state, tileMap->m_vertices);
+    tileMap->load("../res/tiles.png", sf::Vector2u(114, 131), state.map.grid, 7, 7);
 
-    if (!tileMap->load("../res/tiles.png", sf::Vector2u(114, 131), state.map.grid, 7, 7))
-        exit(0);
+    renderPieces = new RenderPieces(state, tileMap->m_vertices);
     // tileMap->setOrigin((8*114)/2, 719/2);
     // tileMap->setPosition(width/2, 30 + height/2);
     // tileMap->setScale(sf::Vector2f(0.9, 0.9));
