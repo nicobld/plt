62b9ec921414e1e48eaa07a6d2506270793486cd
nicobld <belland.nicolas77@gmail.com>
2022-11-09 21:38:42 +0100

    add operator+ to position

---
+++ src/shared/state/Position.cpp
@@ -0,0 +1,16 @@
+#include "Position.h"
+
+namespace state {
+
+Position::Position() {
+    x = 0;
+    y = 0;
+}
+
+Position::Position(int x, int y) : x(x), y(y) {}
+
+Position operator+(Position pos1, Position pos2){
+    return Position(pos1.x + pos2.x, pos1.y + pos2.y);
+}
+
+}
