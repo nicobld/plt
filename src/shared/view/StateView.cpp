d6002ef64c7d0b4511d0828d6c7620264d8d433f
nicobld <belland.nicolas77@gmail.com>
2022-11-09 22:51:52 +0100

    working roads render

--- src/shared/view/StateView.cpp
+++ src/shared/view/StateView.cpp
@@ -9,11 +9,13 @@ StateView::StateView(state::State& state) : state(state){
     displayHUD = new DisplayHUD(width, height, state.players[0], state.players[1], state.players[2], state.players[3]);
     tileMap = new TileMap();
     tileMap->load("../res/tiles.png", sf::Vector2u(114, 131), state.map.grid, 7, 7);
-
-    renderPieces = new RenderPieces(state, tileMap->m_vertices);
+    
     // tileMap->setOrigin((8*114)/2, 719/2);
     // tileMap->setPosition(width/2, 30 + height/2);
     // tileMap->setScale(sf::Vector2f(0.9, 0.9));
+
+    renderPieces = new RenderPieces(state, tileMap->m_vertices);
+
 }
 
 void StateView::render(sf::RenderTarget& target){
