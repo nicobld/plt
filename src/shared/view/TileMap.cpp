244b673055ae3118b511bec83f84910b90a96c28
nicobld <belland.nicolas77@gmail.com>
2022-11-10 15:02:30 +0100

    offset to tilemap

--- src/shared/view/TileMap.cpp
+++ src/shared/view/TileMap.cpp
@@ -9,6 +9,8 @@ bool TileMap::load(const std::string& tileset, sf::Vector2u tileSize, const int*
         return false;
 
     //offset to place map in the middle
+    int offsetX = 1280/7;
+    int offsetY = 0;
 
     // on redimensionne le tableau de vertex pour qu'il puisse contenir tout le niveau
     m_vertices.setPrimitiveType(sf::Triangles);
@@ -26,38 +28,38 @@ bool TileMap::load(const std::string& tileset, sf::Vector2u tileSize, const int*
                 sf::Vertex* hex = &m_vertices[(i + j * width) * 12 + k * 3];
 
                 //la pointe en haut de l'hexagone
-                hex[0].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x, j*0.75 * tileSize.y);
+                hex[0].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x + offsetX, j*0.75 * tileSize.y + offsetY);
                 hex[0].texCoords = sf::Vector2f((tileNumber + 0.5) * tileSize.x, 0);
 
                 //on tourne dans le sens d'une montre pour chercher les 2 prochains points
                 switch (k){
                     case 0:
-                        hex[1].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.25) * tileSize.y);
-                        hex[2].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.75) * tileSize.y);
+                        hex[1].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.25) * tileSize.y + offsetY);
+                        hex[2].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.75) * tileSize.y + offsetY);
 
                         hex[1].texCoords = sf::Vector2f((tileNumber + 1) * tileSize.x, 0.25 * tileSize.y);
                         hex[2].texCoords = sf::Vector2f((tileNumber + 1) * tileSize.x, 0.75 * tileSize.y);
                         break;
                     
                     case 1:
-                        hex[1].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.75) * tileSize.y);
-                        hex[2].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x, (j*0.75 + 1) * tileSize.y);
+                        hex[1].position = sf::Vector2f((i + 1 + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.75) * tileSize.y + offsetY);
+                        hex[2].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 1) * tileSize.y + offsetY);
 
                         hex[1].texCoords = sf::Vector2f((tileNumber + 1) * tileSize.x, 0.75 * tileSize.y);
                         hex[2].texCoords = sf::Vector2f((tileNumber + 0.5) * tileSize.x, 1 * tileSize.y);
                         break;
                     
                     case 2:
-                        hex[1].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x, (j*0.75 + 1) * tileSize.y);
-                        hex[2].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.75) * tileSize.y);
+                        hex[1].position = sf::Vector2f((i + 0.5 + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 1) * tileSize.y + offsetY);
+                        hex[2].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.75) * tileSize.y + offsetY);
 
                         hex[1].texCoords = sf::Vector2f((tileNumber + 0.5) * tileSize.x, 1 * tileSize.y);
                         hex[2].texCoords = sf::Vector2f(tileNumber * tileSize.x, 0.75 * tileSize.y);
                         break;
 
                     case 3:
-                        hex[1].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.75) * tileSize.y);
-                        hex[2].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x, (j*0.75 + 0.25) * tileSize.y);
+                        hex[1].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.75) * tileSize.y + offsetY);
+                        hex[2].position = sf::Vector2f((i + j%2 * 0.5) * tileSize.x + offsetX, (j*0.75 + 0.25) * tileSize.y + offsetY);
 
                         hex[1].texCoords = sf::Vector2f(tileNumber * tileSize.x, 0.75 * tileSize.y);
                         hex[2].texCoords = sf::Vector2f(tileNumber * tileSize.x, 0.25 * tileSize.y);
