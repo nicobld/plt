a184d260fbf396f91bdddf877fd810acf9569aba
nicobld <belland.nicolas77@gmail.com>
2022-11-18 14:44:19 +0100

    commit

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -9,11 +9,13 @@
 
 #include <state.h>
 #include <view.h>
+#include <engine.h>
 
 using namespace std;
 using namespace state;
 using namespace view;
 using namespace sf;
+using namespace engine;
 
 int main(int argc,char* argv[])
 {
@@ -26,7 +28,6 @@ int main(int argc,char* argv[])
 
     if(!strncmp(argv[1], "hello", 5)){
         cout << "===============| Catan |===============" << endl;
-        return 0;
     }
     else if(!strncmp(argv[1], "render", 6)){
         cout << "===============| Catan |===============" << endl;
@@ -83,11 +84,51 @@ int main(int argc,char* argv[])
             // fin de la frame courante, affichage de tout ce qu'on a dessiné
             window.display();
         }
-
-    return 0;
     }
 
     else if(!strncmp(argv[1], "engine", 6)){
-        cout << "===============| Catan |===============" << endl;
+        int width = 1280;
+        int height = 720;
+        sf::RenderWindow window(sf::VideoMode(width, height), "Catan");
+
+        State state;
+        state.players.push_back(Player("Jonah", PlayerRed));
+        state.players.push_back(Player("Nicolas", PlayerBlue));
+        state.players.push_back(Player("Xu", PlayerYellow));
+        state.players.push_back(Player("Stephane", PlayerGreen));
+
+        StateView stateView(state);
+
+        Engine engine;
+
+        PlaceBuildingCommand command(PlayerRed, {Position(1, 3), Position(1, 2), Position(0, 3)});
+        cout << command.execute(&state) << endl;
+        
+        // on fait tourner le programme tant que la fenêtre n'a pas été fermée
+        while (window.isOpen())
+        {
+            // on traite tous les évènements de la fenêtre qui ont été générés depuis la dernière itération de la boucle
+            Event event;
+            while (window.pollEvent(event))
+            {
+                // fermeture de la fenêtre lorsque l'utilisateur le souhaite
+                if (event.type == Event::Closed)
+                    window.close();
+            }
+
+            // effacement de la fenêtre en noir
+            window.clear(Color::Black);
+
+            // c'est ici qu'on dessine tout
+            //HUD.display(window);
+            window.draw(*stateView.tileMap);
+            stateView.displayHUD->render(window, state.players[0], state.players[1], state.players[2], state.players[3]);
+            stateView.render(window);
+
+            // fin de la frame courante, affichage de tout ce qu'on a dessiné
+            window.display();
+        }
     }
+
+    return 0;
 }
--- src/shared/engine/PlaceBuildingCommand.cpp
+++ src/shared/engine/PlaceBuildingCommand.cpp
@@ -1,10 +1,21 @@
 #include "PlaceBuildingCommand.h"
+#include <array>
 
 namespace engine {
 
-PlaceBuildingCommand::PlaceBuildingCommand() {}
+PlaceBuildingCommand::PlaceBuildingCommand(state::PlayerColor playerColor, std::array<state::Position, 3> position) : playerColor(playerColor), position(position) {
+    commandID = PLACE_BUILDING;
+}
 
-bool PlaceBuildingCommand::execute(state::State*) {
+bool PlaceBuildingCommand::execute(state::State* state) {
+    //VERIFY
+    for (state::Building b : state->map.buildings){
+        if (b.position == position) {
+            return false;
+        }
+    }
+
+    
 
 }
 
