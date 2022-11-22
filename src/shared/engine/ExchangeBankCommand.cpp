963656e8a7d624076e7eeb9b0e556bcca912112c
JonahPichard <jnhp29@gmail.com>
2022-11-22 17:17:15 +0100

    ajout echange

--- src/shared/engine/ExchangeBankCommand.cpp
+++ src/shared/engine/ExchangeBankCommand.cpp
@@ -2,8 +2,19 @@
 
 namespace engine {
 
+ExchangeBankCommand::ExchangeBankCommand(state::State* state, state::RessourceType giving, state::RessourceType receiving){
+    commandID = EXCHANGE_BANK;
+}
+
+
 bool ExchangeBankCommand::execute(state::State* state) {
+    
     return true;
 }
+
+
+
+
+
 
 }
