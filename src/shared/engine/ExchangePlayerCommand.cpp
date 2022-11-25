24f14cfe7c91da72777bcfc059ef97bb3d946ef3
JonahPichard <jnhp29@gmail.com>
2022-11-25 10:40:53 +0100

    c'est pas fini

--- src/shared/engine/ExchangePlayerCommand.cpp
+++ src/shared/engine/ExchangePlayerCommand.cpp
@@ -9,35 +9,53 @@ ExchangePlayerCommand::ExchangePlayerCommand(){
     this->givingAsk = givingAsk;
     this->receivingAsk = receivingAsk;
     this playersAsk = playersAsk;
+
+    for(state::Player playerReceiving : state->players){
+        if (playerReceiving.playerColor == state->turn){
+            this->playeReceiving = playerReceviving;
+        }
+    }
     cout << "demande d'échange crée" << to_string(this->givingAsk) << " -> " << to_string(this->receivingAsk) << endl;
 }
 
 bool ExchangePlayerCommand::execute(state::State* state) {
+    for(state::Player playerAsk : playersAsk){
+        verifiyAfterPoprose(state, playerAsk);
 
+
+    }
     return false;
 }
 
-bool ExchangePlayerCommand::verifiy(state::State* state){
+bool ExchangePlayerCommand::verifiyBeforePropose(state::State* state){
+    cout << "ressource du joueur donné avant echange " << to_string(giving) << " : " << playerReceiving.resources[giving].number << endl;
+    cout << "ressource du joueur reçu avant echange " << to_string(receiving) << " : " << playerReceiving.resources[receiving].number << endl;
+    if(playerReceiving.resources[giving].number >= givingNumner){
+        cout << "on peut proposer l'échange" << endl;
+        return true;
     }
-    for(state::Player playerReceiving : state->players){
-        if (playerReceiving.playerColor == state->turn){
-                cout << "ressource du joueur donné avant echange " << to_string(giving) << " : " << playerReceiving.resources[giving].number << endl;
-                cout << "ressource du joueur reçu avant echange " << to_string(receiving) << " : " << playerReceiving.resources[receiving].number << endl;
+    cout << "pas d'échange" << endl;
+    return false;
+}
 
-                cout << "ressource de la banque donné avant echange " << to_string(giving) << " : " << state->gameCards.resources[giving].number  << endl;
-                cout << "ressource de la banque reçu avant echange " << to_string(receiving) << " : " <<  state->gameCards.resources[receiving].number << endl;
-            if(playerReceiving.resources[giving].number >= min && playersAsk[0].resources[receiving].number >= 2){
-                cout << "on peut faire l'échange" << endl;
-                return true;
-            }
-        }
+bool ExchangePlayerCommand::verifiyAfterPoprose(state::State* state, state::¨Player playerAsk){
+    cout << "ressource du joueur " << playerAsk.name << " donné avant echange " << to_string(giving) << " : " << playerAsk.resources[giving].number  << endl;
+    cout << "ressource du joueur " << playerAsk.name << " reçu avant echange " << to_string(receiving) << " : " <<  playerAsk.resources[receiving].number << endl;
+    if(playerAsk.resources[giving].number >= receivingNumber){
+        cout << "on peut faire l'échange" << endl;
+        return true;
     }
     cout << "pas d'échange" << endl;
     return false;
 }
 
 bool ExchangePlayerCommand::exchangeRequest(state::State* state){
+    verifiyBeforePropose(state);
+    for(state::Player playerAsk : playersAsk){
+        
 
+
+    }
     return false;   
 }
 
