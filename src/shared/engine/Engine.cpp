bcf2838cf293b17abbe2a474bf19fd811f5c5b4e
nicobld <belland.nicolas77@gmail.com>
2022-12-06 16:28:22 +0100

    update engine

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -18,26 +18,33 @@ ExchangeRequestCommand* saveExReqCmd;
 
 void Engine::update() {
     for (int i = 0; i < commandQueue.size(); i++){
-        if (commandQueue.front()->playerColor == state->turn){
-            if (state->gameState == NORMAL_STATE){
+
+        if (state->gameState == NORMAL_STATE){
+            if (commandQueue.front()->playerColor == state->turn){
                 if (commandQueue.front()->commandID == EXCHANGE_RESPONSE_CMD); //ne rien faire
-                else if (commandQueue.front()->commandID == EXCHANGE_REQUEST_CMD){
+                else if (commandQueue.front()->commandID == EXCHANGE_REQUEST_CMD){ //passer en exchange state
                     saveExReqCmd = static_cast<ExchangeRequestCommand*>(commandQueue.front());
+                    state->gameState = EXCHANGE_REQUEST_STATE;
                 }
                 else if (commandQueue.front()->verify(state)){
                     commandQueue.front()->execute(state);
+                } else {
+                    std::cout << "Wrong command" << std::endl;
                 }
-            } else if (state->gameState == EXCHANGE_REQUEST_STATE){
-                if (commandQueue.front()->commandID == EXCHANGE_RESPONSE_CMD){ //seulement accepter les reponses
-                    if (commandQueue.front()->verify(state)){
-                        commandQueue.front()->execute(state);
-                    }
+            }
+        } 
+        
+        else if (state->gameState == EXCHANGE_REQUEST_STATE){
+            if (commandQueue.front()->commandID == EXCHANGE_RESPONSE_CMD){ //seulement accepter les reponses
+                if (commandQueue.front()->verify(state)){
+                    commandQueue.front()->execute(state);
+                } else {
+                    std::cout << "Wrong command" << std::endl;
                 }
             }
         }
-        else 
-            std::cout << "Wrong command" << std::endl;
-        
+
+
         delete commandQueue.front();
         commandQueue.pop();
     }
