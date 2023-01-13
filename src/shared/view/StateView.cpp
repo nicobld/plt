d86a31b5a4ead31ad953574d82b4d9ff18ef4dfd
JonahPichard <jnhp29@gmail.com>
2023-01-13 17:31:32 +0100

    bug menu echnage resolu

--- src/shared/view/StateView.cpp
+++ src/shared/view/StateView.cpp
@@ -204,8 +204,8 @@ void StateView::updateClickableObjects(state::PlayerColor playerColor)
     handPlayers[viewPlayer].updateHand();
     char s[64];
     static int menuResClicks = 0;
-    state::Resource giving;
-    state::Resource receiving;
+    static state::Resource giving;
+    static state::Resource receiving;
     std::cout << "UPDATE CLICK OBJECT\n";
     // if (state->turn != viewPlayer){
     //     viewPlayer = state->turn;
@@ -356,6 +356,7 @@ void StateView::updateClickableObjects(state::PlayerColor playerColor)
         break;
 
     case ACCEPT_EXCHANGE:
+        std::cout << "giving type : " << resTypeToString(giving.resourceType) << " | giving number : " << giving.number <<std::endl;
         clickableMenu.push_back((Menu *)new MenuAcceptExchange(state, *menuTexture, state->turn, giving, receiving, sf::IntRect(1280 / 2 - 351 / 2, 720 - 205, 351, 205), &(displayState[viewPlayer])));
         clickableButton.push_back((Button *)((MenuAcceptExchange *)clickableMenu.back())->buttonValidate);
         clickableButton.push_back((Button *)((MenuAcceptExchange *)clickableMenu.back())->buttonDeny);
